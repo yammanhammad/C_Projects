07cba73ea64d61eed4b9f98bd05bf0ffd14ba8cf
Muhammad Yamman Hammad <muhammad.61596@iqra.edu.pk>
2025-03-12 15:04:31 +0500

    Added Hello World program

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Standard Input Output Library
+
+int main() {
+    printf("Hello, World!\n"); // Print message to the console
+    return 0; // Indicate successful execution
+}
