99efa4d3662dd30700ac7f604f6b281efefcc5e7
Muhammad Yamman Hammad <muhammad.61596@iqra.edu.pk>
2025-03-23 07:53:05 +0500

    Added Increasing Odd Rows program

---
+++ increasing_odd_rows.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int r,c;
+    for(r=1; r<=7;r+=2)
+    {     
+        for(c=1;c<=r;c++)
+        {     
+            printf("%d", c);
+        }
+        printf("\n");
+    }    
+    return 0;
+}
