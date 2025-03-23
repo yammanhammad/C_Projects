e12359231e810707cce6a3a6383fb508da5e2319
Muhammad Yamman Hammad <muhammad.61596@iqra.edu.pk>
2025-03-23 07:15:58 +0500

    Added Odd Number Triangle program

---
+++ odd_number_triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int r,c;
+
+    for(r=1; r<=7;r+=2)
+    {   
+        for(c=1;c<=r;c+=2)
+        {     
+            printf("%d  ",c);
+        }
+        printf("\n");
+    }    
+    return 0;
+}
